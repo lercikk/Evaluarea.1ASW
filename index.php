dea5f94f2dab6696655d69d9c68576bf86139f8f
lercikk <sinigur.valerica@elev.cihcahul.md>
2024-02-23 10:57:26 +0200

    Evaluarea.1

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+echo "Bine ați venit pe pagina lui Sinigur Valeria!";
+?>
+
+// index.html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Evaluarea 1</title>
+</head>
+<body>
+    <h1>Bine ați venit pe pagina mea!</h1>
+</body>
+</html>
